8bb0a10bf1b3da86e74058fb4ee77c4a1ede01f8
camiloo01 <149196896+camiloo01@users.noreply.github.com>
2024-04-08 01:47:36 -0500

    Add files via upload

---
+++ models/donadorModel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const donadorSchema = new mongoose.Schema({
+  nombre: {
+    type: String,
+    required: true
+  },
+  correo: {
+    type: String,
+    required: true
+  },
+  telefono: {
+    type: String,
+    required: true
+  },
+  edad: {
+    type: Number,
+    required: true
+  }
+});
+
+const Donador = mongoose.model('Donador', donadorSchema);
+
+module.exports = Donador;
